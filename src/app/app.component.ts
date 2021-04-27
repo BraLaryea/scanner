dbb7d9e9f70dbf284533b73e850fb70e9ef0c49a
bralaryea <laryea.enl99@gmail.com>
2021-04-27 18:30:03 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { BarcodeFormat } from '@zxing/library';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qrscan';
+  value = "";
+  allowedFormats = [BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX /*, ...*/];
+
+  Output: string | undefined;
+
+  onScan(output: string) {
+    this.Output = output
+  };
+
+  generate(inputed: string) {
+    this.value = inputed
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ZXingScannerModule } from '@zxing/ngx-scanner'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgxBarcodeModule } from 'ngx-barcode';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZXingScannerModule,
+    NgxBarcodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
